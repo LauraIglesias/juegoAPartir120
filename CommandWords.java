c81cd86cbe29211b86e96b9e4b9d5561d0f0eb3b
Laura <lalacdll@hotmail.com>
2014-05-16 13:05:14 +0200

    Relaccionamos los comandos con el enum: declarado hasMap y inicializado

--- CommandWords.java
+++ CommandWords.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 /**
  * This class is part of the "World of Zuul" application. 
  * "World of Zuul" is a very simple, text based adventure game.  
@@ -15,19 +16,31 @@ public class CommandWords
     private static final String[] validCommands = {
             "go", "quit", "help", "look", "eat", "back", "items","take","drop"
         };
+    private HashMap<String, Option> comandos;
 
     /**
      * Constructor - initialise the command words.
      */
     public CommandWords()
     {
-        // nothing to do at the moment...
+        comandos = new HashMap<>();
+        comandos.put("go", Option.GO);
+        comandos.put("quit", Option.QUIT);
+        comandos.put("help", Option.HELP);
+        comandos.put("look", Option.LOOK);
+        comandos.put("eat", Option.EAT);
+        comandos.put("back", Option.BACK);
+        comandos.put("items", Option.ITEMS);
+        comandos.put("take", Option.TAKE);
+        comandos.put("drop", Option.DROP);
     }
 
     /**
      * Check whether a given String is a valid command word. 
      * @return true if a given string is a valid command,
      * false if it isn't.
+     * Comprobar si una cadena dada es una palabra de comando válida.
+     * @ Return true si una cadena dada es un comando válido, false si no lo es.
      */
     public boolean isCommand(String aString)
     {
@@ -41,6 +54,7 @@ public class CommandWords
 
     /**
      * Print all valid commands to System.out.
+     * muestre en la terminal todos los comandos disponibles
      */    
     public void showAll()
     {
